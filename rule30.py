6714f7236469b7f8caba5c5eba81fb8e78040011
CitrusPunk <marian.kloesler@gmx.de>
2016-05-04 23:29:07 +0200

    add: rule30.py, first 1d cellular automaton in python

---
+++ rule30.py
@@ -0,0 +1,35 @@
+#rule30
+from pref import *
+
+def getNextGen(old = []):
+	nextGen = old[:]
+	
+	#left border
+	num = 0
+	if(old[0] == 1): num += 2
+	if(old[1] == 1): num += 1
+	nextGen[0] = rule[num]
+
+	#"body"-field
+	for i in range(1,len(old)-1):
+		num = 0	
+		if(old[i-1] == 1): num += 3
+		if(old[i] == 1): num += 2
+		if(old[i+1] == 1): num += 1
+		nextGen[i] = rule[num]
+
+	#right border
+	num = 0
+	if(old[len(old)-2] == 1): num += 3
+	if(old[len(old)-1] == 1): num += 2
+	nextGen[len(old)-1] = rule[num]
+
+	return nextGen
+
+if (__name__ == "__main__"):
+	
+	while True:
+		print ''.join(mapping[s] for s in start)
+		raw_input()
+		new = getNextGen(start)
+		start = new
